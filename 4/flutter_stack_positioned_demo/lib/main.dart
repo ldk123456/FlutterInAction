b220782077e5e0c72bbed418929352edf2b7b086
刘德凯 <dekai.liu@ximalaya.com>
2019-09-30 09:35:35 +0800

    层叠布局：Stack、Positioned

---
+++ 4/flutter_stack_positioned_demo/lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter 层叠布局'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: ConstrainedBox(
+          // 确保Stack占满屏幕
+          constraints: BoxConstraints.expand(),
+          child: Stack(
+            alignment: Alignment.center,  // 指定未定位或部分定位widget的对齐方式
+            fit: StackFit.expand,   // 未定位widget占满Stack整个空间
+            children: <Widget>[
+              Positioned(
+                left: 20.0,
+                child: Text("I am Jack"),
+              ),
+              Container(
+                child: Text("Hello World", style: TextStyle(color: Colors.white)),
+                color: Colors.red,
+              ),
+              Positioned(
+                top: 20.0,
+                child: Text("Your Friend"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
