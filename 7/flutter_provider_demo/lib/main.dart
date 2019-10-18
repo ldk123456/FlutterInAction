e6d89c5e0274e50712904f915f4166b819cd53d8
liudekai <dekai.liu@ximalaya.com>
2019-10-18 17:50:09 +0800

    跨组件状态共享：Provider

---
+++ 7/flutter_provider_demo/lib/main.dart
@@ -0,0 +1,209 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter 跨组件状态共享'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: ProviderRoute(),
+    );
+  }
+}
+
+class Item{
+  double price;   // 商品单价
+  int count;    // 商品份数
+  Item(this.price, this.count);
+}
+
+class CartModel extends ChangeNotifier {
+  // 用于保存购物车中商品列表
+  final List<Item> _items = [];
+
+  // 禁止改变购物车里的商品信息
+  UnmodifiableListView<Item> get items => UnmodifiableListView(_items);
+
+  // 购物车中商品的总价
+  double get totalPrice => _items.fold(0, (value, item) => value + item.count * item.price);
+
+  // 将 [item] 添加到购物车。这是唯一一种能从外部改变购物车的方法。
+  void add(Item item) {
+    _items.add(item);
+    // 通知监听器（订阅者），重新构建InheritedProvider， 更新状态
+    notifyListeners();
+  }
+}
+
+class ProviderRoute extends StatefulWidget {
+  @override
+  _ProviderRouteState createState() => new _ProviderRouteState();
+}
+
+class _ProviderRouteState extends State<ProviderRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ChangeNotifierProvider<CartModel>(
+        data: CartModel(),
+        child: Builder(builder: (context) {
+          return Column(
+            children: <Widget>[
+              Builder(builder: (context){
+                var cart=ChangeNotifierProvider.of<CartModel>(context);
+                return Text("总价: ${cart.totalPrice}");
+              }),
+              Builder(builder: (context){
+                print("RaisedButton build"); //在后面优化部分会用到
+                return RaisedButton(
+                  child: Text("添加商品"),
+                  onPressed: () {
+                    //给购物车中添加商品，添加后总价会更新
+                    ChangeNotifierProvider.of<CartModel>(context).add(Item(20.0, 1));
+                  },
+                );
+              }),
+            ],
+          );
+        }),
+      ),
+    );
+  }
+}
+
+// 一个通用的InheritedWidget，保存需要跨组件共享的状态
+class InheritedProvider<T> extends InheritedWidget {
+
+  // 共享状态使用泛型
+  final T data;
+
+  InheritedProvider({
+    @required this.data,
+    Widget child
+  }) : super(child: child);
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    // 在此简单返回true，则每次更新都会调用依赖其的子孙节点的`didChangeDependencies`
+    return true;
+  }
+}
+
+// 该方法用于在Dart中获取模板类型
+Type _typeOf<T>() => T;
+
+class ChangeNotifierProvider<T extends ChangeNotifier> extends StatefulWidget {
+
+  final Widget child;
+  final T data;
+
+  ChangeNotifierProvider({
+    Key key,
+    this.data,
+    this.child,
+  });
+
+  // 定义一个便捷方法，方便子树中的widget获取共享数据
+  static T of<T>(BuildContext context) {
+    final type = _typeOf<InheritedProvider<T>>();
+    final provider = context.inheritFromWidgetOfExactType(type) as InheritedProvider<T>;
+    return provider.data;
+  }
+
+  @override
+  _ChangeNotifierProviderState<T> createState() => new _ChangeNotifierProviderState<T>();
+}
+
+class _ChangeNotifierProviderState<T extends ChangeNotifier> extends State<ChangeNotifierProvider<T>> {
+  void update() {
+    // 如果数据发生变化（model类调用了notifyListeners），重新构建InheritedProvider
+    setState(() => {});
+  }
+
+  @override
+  void didUpdateWidget(ChangeNotifierProvider<T> oldWidget) {
+    // 当Provider更新时，如果新旧数据不相同，则解绑旧数据监听，同时添加新数据监听
+    if (widget.data != oldWidget.data) {
+      oldWidget.data.removeListener(update);
+      widget.data.addListener(update);
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void initState() {
+    // 给model添加监听器
+    widget.data.addListener(update);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // 移除model的监听器
+    widget.data.removeListener(update);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InheritedProvider<T>(
+      data: widget.data,
+      child: widget.child,
+    );
+  }
+}
