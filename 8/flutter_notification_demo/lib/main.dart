bf72141f58539855929701f4554e76d67cc22ff1
liudekai <dekai.liu@ximalaya.com>
2019-10-22 17:31:37 +0800

    通知：Notification

---
+++ 8/flutter_notification_demo/lib/main.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter 通知'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: NotificationRoute(),
+      /*NotificationListener(
+        onNotification: (notification) {
+          switch (notification.runtimeType) {
+            case ScrollStartNotification: print("开始滚动"); break;
+            case ScrollUpdateNotification: print("正在滚动"); break;
+            case ScrollEndNotification: print("滚动停止"); break;
+            case OverscrollNotification: print("滚动到边界"); break;
+            default: break;
+          }
+          return true;
+        },
+        child: ListView.builder(
+          itemCount: 100,
+          itemBuilder: (context, index) {
+            return ListTile(title: Text("$index"));
+          },
+        ),
+      ),*/
+    );
+  }
+}
+
+class MyNotification extends Notification {
+  final String msg;
+  
+  MyNotification(this.msg);
+}
+
+class NotificationRoute extends StatefulWidget {
+  @override
+  NotificationRouteState createState() => new NotificationRouteState();
+}
+
+class NotificationRouteState extends State<NotificationRoute> {
+  String _msg = "";
+
+  @override
+  Widget build(BuildContext context) {
+    // 监听通知
+    return NotificationListener<MyNotification>(
+      onNotification: (notification){
+        print(notification.msg); // 打印通知
+        return false;
+      },
+      child: NotificationListener<MyNotification>(
+        onNotification: (notification) {
+          setState(() {
+            _msg += notification.msg + " ";
+          });
+          return false;
+        },
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Builder(
+                builder: (context) {
+                  return RaisedButton(
+                    //按钮点击时分发通知
+                    onPressed: () => MyNotification("Hi").dispatch(context),
+                    child: Text("Send Notification"),
+                  );
+                },
+              ),
+              Text(_msg),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
