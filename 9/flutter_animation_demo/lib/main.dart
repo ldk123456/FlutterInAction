a7725a9b4068f63f088b361fad4f0d66be3e22b8
liudekai <dekai.liu@ximalaya.com>
2019-10-23 20:06:57 +0800

    Flutter动画

---
+++ 9/flutter_animation_demo/lib/main.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter 动画简介'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: ScaleAnimationRoute(),
+    );
+  }
+}
+
+class AnimatedImage extends AnimatedWidget {
+  AnimatedImage({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = listenable;
+    return Center(
+      child: Image.asset(
+        "images/avatar.png",
+        width: animation.value,
+        height: animation.value,
+      ),
+    );
+  }
+}
+
+class ScaleAnimationRoute extends StatefulWidget {
+  @override
+  _ScaleAnimationRouteState createState() => new _ScaleAnimationRouteState();
+}
+
+//需要继承TickerProvider，如果有多个AnimationController，则应该使用TickerProviderStateMixin。
+class _ScaleAnimationRouteState extends State<ScaleAnimationRoute>
+    with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = new AnimationController(
+        duration: const Duration(seconds: 3), vsync: this);
+    // 使用弹性曲线
+//    animation = CurvedAnimation(parent: controller, curve: Curves.bounceIn);
+    // 图片宽高从0变到300
+    animation = new Tween(begin: 0.0, end: 300.0).animate(controller);
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        // 动画执行结束时反向执行动画
+        controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        // 动画恢复到初始状态时执行动画（正向）
+        controller.forward();
+      }
+    });
+    // 启动动画(正向执行)
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+//    return AnimatedImage(animation: animation);
+    /*return AnimatedBuilder(
+      animation: animation,
+      child: Image.asset("images/avatar.png"),
+      builder: (BuildContext context, Widget child) {
+        return Center(
+          child: Container(
+            height: animation.value,
+            width: animation.value,
+            child: child,
+          ),
+        );
+      },
+    );*/
+    return GrowTransition(
+      child: Image.asset("images/avatar.png"),
+      animation: animation,
+    );
+  }
+
+  @override
+  void dispose() {
+    // 路由销毁时需要释放动画资源
+    controller.dispose();
+    super.dispose();
+  }
+}
+
+class GrowTransition extends StatelessWidget {
+  final Widget child;
+  final Animation<double> animation;
+
+  GrowTransition({this.child, this.animation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AnimatedBuilder(
+        animation: animation,
+        builder: (BuildContext context, Widget child) {
+          return Container(
+            height: animation.value,
+            width: animation.value,
+            child: child,
+          );
+        },
+        child: child,
+      ),
+    );
+  }
+}
