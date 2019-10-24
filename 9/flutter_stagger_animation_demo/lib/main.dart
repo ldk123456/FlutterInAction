1fd49ea12eb43de19e64c15bca8056888da7dd8b
liudekai <dekai.liu@ximalaya.com>
2019-10-24 10:24:57 +0800

    交织动画：StaggerAnimation

---
+++ 9/flutter_stagger_animation_demo/lib/main.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter 交织动画'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: StaggerRoute(),
+    );
+  }
+}
+
+class StaggerAnimation extends StatelessWidget {
+  final Animation<double> controller;
+  Animation<double> height;
+  Animation<EdgeInsets> padding;
+  Animation<Color> color;
+
+  StaggerAnimation({Key key, this.controller}) : super(key: key) {
+    // 高度动画
+    height = Tween<double>(
+      begin: 0.0,
+      end: 300.0,
+    ).animate(CurvedAnimation(
+      parent: controller,
+      curve: Interval(
+        0.0, 0.6, // 间隔，前60%的动画时间
+        curve: Curves.ease,
+      ),
+    ));
+
+    // 颜色动画
+    color = ColorTween(
+      begin: Colors.green,
+      end: Colors.red,
+    ).animate(CurvedAnimation(
+      parent: controller,
+      curve: Interval(
+        0.0, 0.6, // 间隔，前60%的动画时间
+        curve: Curves.ease,
+      ),
+    ));
+
+    // 平移动画
+    padding = Tween<EdgeInsets>(
+      begin: EdgeInsets.only(left: 0.0),
+      end: EdgeInsets.only(left: 100.0),
+    ).animate(CurvedAnimation(
+      parent: controller,
+      curve: Interval(
+        0.6, 1.0, // 间隔，后40%的动画时间
+        curve: Curves.ease,
+      ),
+    ));
+  }
+
+  Widget _buildAnimation(BuildContext context, Widget child) {
+    return Container(
+      alignment: Alignment.bottomCenter,
+      padding: padding.value,
+      child: Container(
+        color: color.value,
+        width: 50.0,
+        height: height.value,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      builder: _buildAnimation,
+      animation: controller,
+    );
+  }
+}
+
+class StaggerRoute extends StatefulWidget {
+  @override
+  _StaggerRouteState createState() => new _StaggerRouteState();
+}
+
+class _StaggerRouteState extends State<StaggerRoute>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 2000),
+      vsync: this,
+    );
+  }
+
+  Future<Null> _playAnimation() async {
+    try {
+      // 先正向执行动画
+      await _controller.forward().orCancel;
+      // 再反向执行动画
+      await _controller.reverse().orCancel;
+    } on TickerCanceled {
+      // the animation got canceled, probably because we were disposed
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        _playAnimation();
+      },
+      child: Center(
+        child: Container(
+          width: 300.0,
+          height: 300.0,
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.1),
+            border: Border.all(
+              color: Colors.black.withOpacity(0.5),
+            ),
+          ),
+          child: StaggerAnimation(
+            controller: _controller,
+          ),
+        ),
+      ),
+    );
+  }
+}
