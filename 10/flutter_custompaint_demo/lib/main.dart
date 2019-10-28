fa759838ae64d68cc5e49b5809a9608f3641ce47
liudekai <dekai.liu@ximalaya.com>
2019-10-28 14:21:22 +0800

    自绘组件：CustomPaint与Canvas

---
+++ 10/flutter_custompaint_demo/lib/main.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter 自绘组件'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: CustomPaintRoute(),
+    );
+  }
+}
+
+class CustomPaintRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CustomPaint(
+        size: Size(300, 300), // 指定画布大小
+        painter: MyPainter(),
+      ),
+    );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    double eWidth = size.width / 15;
+    double eHeight = size.height / 15;
+
+    //画棋盘背景
+    var paint = Paint() // 创建一个画笔并配置其属性
+      ..isAntiAlias = true // 是否抗锯齿
+      ..style = PaintingStyle.fill // 画笔样式：填充
+      ..color = Color(0xffcdb175); // 背景为纸黄色
+    canvas.drawRect(Offset.zero & size, paint);
+
+    //画棋盘网络
+    paint
+      ..style = PaintingStyle.stroke // 线
+      ..color = Colors.black87
+      ..strokeWidth = 1.0;
+
+    for (int i = 0; i <= 15; ++i) {
+      double dy = eHeight * i;
+      canvas.drawLine(Offset(0, dy), Offset(size.width, dy), paint);
+    }
+
+    for (int i = 0; i <= 15; ++i) {
+      double dx = eWidth * i;
+      canvas.drawLine(Offset(dx, 0), Offset(dx, size.height), paint);
+    }
+
+    // 画一个黑子
+    paint
+      ..style = PaintingStyle.fill
+      ..color = Colors.black;
+    canvas.drawCircle(
+      Offset(size.width / 2 - eWidth / 2, size.height / 2 - eHeight / 2),
+      min(eWidth/2, eHeight/2),
+      paint
+    );
+
+    // 画一个白子
+    paint
+      ..style = PaintingStyle.fill
+      ..color = Colors.white;
+    canvas.drawCircle(
+        Offset(size.width / 2 + eWidth / 2, size.height / 2 - eHeight / 2),
+        min(eWidth/2, eHeight/2),
+        paint
+    );
+  }
+
+  // 在实际场景中正确利用此回调可以避免重绘开销，本示例我们简单的返回true
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
